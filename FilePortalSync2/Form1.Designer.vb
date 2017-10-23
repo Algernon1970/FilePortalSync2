cb508acfbe349cff0b009084ef9f6b346efbd02b
p-page <p-page@ashbyschool.org.uk>
2017-10-23 10:53:41 +0100

    InitialSubmit - general outline done.

--- FilePortalSync2/Form1.Designer.vb
+++ FilePortalSync2/Form1.Designer.vb
@@ -22,9 +22,220 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.SetupGroupBox = New System.Windows.Forms.GroupBox()
+        Me.LocalInspectButton = New System.Windows.Forms.Button()
+        Me.LibraryInspectButton = New System.Windows.Forms.Button()
+        Me.LocalShareBox = New System.Windows.Forms.TextBox()
+        Me.LibraryShareBox = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.StarterButton = New System.Windows.Forms.Button()
+        Me.PasswordBox = New System.Windows.Forms.TextBox()
+        Me.UsernameBox = New System.Windows.Forms.TextBox()
+        Me.PasswordLabel = New System.Windows.Forms.Label()
+        Me.UsernameLabel = New System.Windows.Forms.Label()
+        Me.OutputGroupBox = New System.Windows.Forms.GroupBox()
+        Me.EventLogging = New System.Diagnostics.EventLog()
+        Me.OutputBox = New System.Windows.Forms.RichTextBox()
+        Me.TableLayoutPanel1.SuspendLayout()
+        Me.SetupGroupBox.SuspendLayout()
+        Me.OutputGroupBox.SuspendLayout()
+        CType(Me.EventLogging, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 1
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle())
+        Me.TableLayoutPanel1.Controls.Add(Me.SetupGroupBox, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.OutputGroupBox, 0, 1)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(1070, 508)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'SetupGroupBox
+        '
+        Me.SetupGroupBox.Controls.Add(Me.LocalInspectButton)
+        Me.SetupGroupBox.Controls.Add(Me.LibraryInspectButton)
+        Me.SetupGroupBox.Controls.Add(Me.LocalShareBox)
+        Me.SetupGroupBox.Controls.Add(Me.LibraryShareBox)
+        Me.SetupGroupBox.Controls.Add(Me.Label2)
+        Me.SetupGroupBox.Controls.Add(Me.Label1)
+        Me.SetupGroupBox.Controls.Add(Me.StarterButton)
+        Me.SetupGroupBox.Controls.Add(Me.PasswordBox)
+        Me.SetupGroupBox.Controls.Add(Me.UsernameBox)
+        Me.SetupGroupBox.Controls.Add(Me.PasswordLabel)
+        Me.SetupGroupBox.Controls.Add(Me.UsernameLabel)
+        Me.SetupGroupBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SetupGroupBox.Location = New System.Drawing.Point(3, 3)
+        Me.SetupGroupBox.Name = "SetupGroupBox"
+        Me.SetupGroupBox.Size = New System.Drawing.Size(1064, 94)
+        Me.SetupGroupBox.TabIndex = 0
+        Me.SetupGroupBox.TabStop = False
+        Me.SetupGroupBox.Text = "Setup"
+        '
+        'LocalInspectButton
+        '
+        Me.LocalInspectButton.Location = New System.Drawing.Point(747, 44)
+        Me.LocalInspectButton.Name = "LocalInspectButton"
+        Me.LocalInspectButton.Size = New System.Drawing.Size(75, 23)
+        Me.LocalInspectButton.TabIndex = 10
+        Me.LocalInspectButton.Text = "Inspect"
+        Me.LocalInspectButton.UseVisualStyleBackColor = True
+        '
+        'LibraryInspectButton
+        '
+        Me.LibraryInspectButton.Location = New System.Drawing.Point(747, 19)
+        Me.LibraryInspectButton.Name = "LibraryInspectButton"
+        Me.LibraryInspectButton.Size = New System.Drawing.Size(75, 23)
+        Me.LibraryInspectButton.TabIndex = 9
+        Me.LibraryInspectButton.Text = "Inspect"
+        Me.LibraryInspectButton.UseVisualStyleBackColor = True
+        '
+        'LocalShareBox
+        '
+        Me.LocalShareBox.Location = New System.Drawing.Point(422, 46)
+        Me.LocalShareBox.Name = "LocalShareBox"
+        Me.LocalShareBox.Size = New System.Drawing.Size(319, 20)
+        Me.LocalShareBox.TabIndex = 8
+        Me.LocalShareBox.Text = "\\192.168.5.57\public\"
+        '
+        'LibraryShareBox
+        '
+        Me.LibraryShareBox.Location = New System.Drawing.Point(422, 20)
+        Me.LibraryShareBox.Name = "LibraryShareBox"
+        Me.LibraryShareBox.Size = New System.Drawing.Size(319, 20)
+        Me.LibraryShareBox.TabIndex = 7
+        Me.LibraryShareBox.Text = "https://ashbyschool.sharepoint.com/StaffShared/AshbyShare"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(353, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Local Share"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(353, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Library URL"
+        '
+        'StarterButton
+        '
+        Me.StarterButton.BackColor = System.Drawing.Color.Lime
+        Me.StarterButton.Location = New System.Drawing.Point(980, 71)
+        Me.StarterButton.Name = "StarterButton"
+        Me.StarterButton.Size = New System.Drawing.Size(75, 23)
+        Me.StarterButton.TabIndex = 4
+        Me.StarterButton.Text = "Start"
+        Me.StarterButton.UseVisualStyleBackColor = False
+        '
+        'PasswordBox
+        '
+        Me.PasswordBox.Location = New System.Drawing.Point(100, 46)
+        Me.PasswordBox.Name = "PasswordBox"
+        Me.PasswordBox.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.PasswordBox.Size = New System.Drawing.Size(186, 20)
+        Me.PasswordBox.TabIndex = 3
+        Me.PasswordBox.Text = "tkcdx7jiywmbYfmsxKTq"
+        Me.PasswordBox.UseSystemPasswordChar = True
+        '
+        'UsernameBox
+        '
+        Me.UsernameBox.Location = New System.Drawing.Point(100, 20)
+        Me.UsernameBox.Name = "UsernameBox"
+        Me.UsernameBox.Size = New System.Drawing.Size(186, 20)
+        Me.UsernameBox.TabIndex = 2
+        Me.UsernameBox.Text = "sharepointuser@ashbyschool.org.uk"
+        '
+        'PasswordLabel
+        '
+        Me.PasswordLabel.AutoSize = True
+        Me.PasswordLabel.Location = New System.Drawing.Point(9, 49)
+        Me.PasswordLabel.Name = "PasswordLabel"
+        Me.PasswordLabel.Size = New System.Drawing.Size(83, 13)
+        Me.PasswordLabel.TabIndex = 1
+        Me.PasswordLabel.Text = "Portal Password"
+        '
+        'UsernameLabel
+        '
+        Me.UsernameLabel.AutoSize = True
+        Me.UsernameLabel.Location = New System.Drawing.Point(9, 23)
+        Me.UsernameLabel.Name = "UsernameLabel"
+        Me.UsernameLabel.Size = New System.Drawing.Size(85, 13)
+        Me.UsernameLabel.TabIndex = 0
+        Me.UsernameLabel.Text = "Portal Username"
+        '
+        'OutputGroupBox
+        '
+        Me.OutputGroupBox.Controls.Add(Me.OutputBox)
+        Me.OutputGroupBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.OutputGroupBox.Location = New System.Drawing.Point(3, 103)
+        Me.OutputGroupBox.Name = "OutputGroupBox"
+        Me.OutputGroupBox.Size = New System.Drawing.Size(1064, 402)
+        Me.OutputGroupBox.TabIndex = 1
+        Me.OutputGroupBox.TabStop = False
+        Me.OutputGroupBox.Text = "Output"
+        '
+        'EventLogging
+        '
+        Me.EventLogging.EnableRaisingEvents = True
+        Me.EventLogging.Log = "HandinPortal"
+        Me.EventLogging.Source = "FileportalSync2"
+        Me.EventLogging.SynchronizingObject = Me
+        '
+        'OutputBox
+        '
+        Me.OutputBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.OutputBox.Location = New System.Drawing.Point(3, 16)
+        Me.OutputBox.Name = "OutputBox"
+        Me.OutputBox.Size = New System.Drawing.Size(1058, 383)
+        Me.OutputBox.TabIndex = 0
+        Me.OutputBox.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(1070, 508)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "Form1"
+        Me.Text = "FilePortalSync2"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.SetupGroupBox.ResumeLayout(False)
+        Me.SetupGroupBox.PerformLayout()
+        Me.OutputGroupBox.ResumeLayout(False)
+        CType(Me.EventLogging, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents SetupGroupBox As GroupBox
+    Friend WithEvents LibraryShareBox As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents StarterButton As Button
+    Friend WithEvents PasswordBox As TextBox
+    Friend WithEvents UsernameBox As TextBox
+    Friend WithEvents PasswordLabel As Label
+    Friend WithEvents UsernameLabel As Label
+    Friend WithEvents OutputGroupBox As GroupBox
+    Friend WithEvents LocalInspectButton As Button
+    Friend WithEvents LibraryInspectButton As Button
+    Friend WithEvents LocalShareBox As TextBox
+    Friend WithEvents EventLogging As EventLog
+    Friend WithEvents OutputBox As RichTextBox
 End Class
--- FilePortalSync2/Form1.vb
+++ FilePortalSync2/Form1.vb
@@ -1,3 +1,72 @@
-﻿Public Class Form1
+﻿Imports System.ComponentModel
+
+Public Class Form1
+    Dim cancelled As Boolean = True
+    Dim worker As New BackgroundWorker
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        worker.WorkerSupportsCancellation = True
+        AddHandler worker.DoWork, AddressOf Worker_DoWork
+    End Sub
+
+    Private Sub LibraryInspectButton_Click(sender As Object, e As EventArgs) Handles LibraryInspectButton.Click
+        Dim processProperties As New ProcessStartInfo With {
+            .FileName = "C:\windows\explorer.exe",
+            .Arguments = String.Format("/n /e /root,{0}/shares", LibraryShareBox.Text)
+        }
+        Dim myProcess As Process = Process.Start(processProperties)
+    End Sub
+
+    Private Sub LocalInspectButton_Click(sender As Object, e As EventArgs) Handles LocalInspectButton.Click
+        Dim processProperties As New ProcessStartInfo With {
+            .FileName = "C:\windows\explorer.exe",
+            .Arguments = String.Format("/n /e /root,{0}", LocalShareBox.Text)
+        }
+        Dim myProcess As Process = Process.Start(processProperties)
+    End Sub
+
+    Private Sub EventLogging_EntryWritten(sender As Object, e As EntryWrittenEventArgs) Handles EventLogging.EntryWritten
+        OutputBox.AppendText(String.Format("{1}{0}{2}{0}{3}", vbTab, e.Entry.TimeGenerated, e.Entry.Message, vbCrLf))
+    End Sub
+
+    Private Sub StarterButton_Click(sender As Object, e As EventArgs) Handles StarterButton.Click
+        If cancelled Then
+            cancelled = False
+            StarterButton.Text = "Stop"
+            StarterButton.BackColor = Color.Red
+            worker.RunWorkerAsync()
+        Else
+            cancelled = True
+            worker.CancelAsync()
+            StarterButton.Text = "Start"
+            StarterButton.BackColor = Color.Green
+        End If
+
+    End Sub
+
+    Private Sub Worker_DoWork(ByVal sender As Object, ByVal e As DoWorkEventArgs)
+        While Not cancelled
+            UploadFiles()
+            DeleteFiles()
+            DownloadFiles()
+            Threading.Thread.Sleep(2500)
+        End While
+    End Sub
+
+#Region "Sharepoint"
+    Private Sub UploadFiles()
+
+    End Sub
+
+    Private Sub DeleteFiles()
+
+    End Sub
+
+    Private Sub DownloadFiles()
+
+    End Sub
+#End Region
+
+
 
 End Class
